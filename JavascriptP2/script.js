d372c158ca5fa775401de8906aef6e0d7bc60017
JaynesN <jaynesn763@my.clevelandcc.edu>
2024-12-04 10:08:21 -0500

    Add files via upload

---
+++ JavascriptP2/script.js
@@ -0,0 +1,14 @@
+font1="5px"
+font2="20px"
+font3="300px"
+function smallText() {
+    document.getElementById("p1").style.fontSize = font1;
+    }
+    
+    function regularText() {
+    document.getElementById("p1").style.fontSize = font2;
+    }
+    
+    function biggerText() {
+    document.getElementById("p1").style.fontSize = font3;
+    }
